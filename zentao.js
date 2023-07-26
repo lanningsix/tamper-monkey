d670a3e90a55c548d0359b90a7f553d9439f5a90
lanningsix <1055194247@qq.com>
2023-07-26 16:15:57 +0800

    Update zentao.js

--- zentao.js
+++ zentao.js
@@ -1,45 +1,18 @@
 // ==UserScript==
-// @name         zentao
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  try to take over the world!
-// @author       You
-// @match        zentao.tangees.com/*
-// @grant        GM_xmlhttpRequest
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=tangees.com
+// @name        bilibili直播弹幕修复
+// @namespace   https://github.com/lanningsix/tamper-monkey
+// @version     0.0.1
+// @author      lzghzr
+// @description 修复bilibili直播弹幕
+// @supportURL  https://github.com/lzghzr/TampermonkeyJS/issues
+// @include     /^https:\/\/live\.bilibili\.com\/(?:blanc\/)?\d/
+// @require     https://fastly.jsdelivr.net/gh/lzghzr/TampermonkeyJS@e4c2e5a14b55a5a0a3fab43f206496cb30c9b6e6/bliveproxy/bliveproxy.js
+// @license     MIT
+// @run-at      document-start
 // ==/UserScript==
-
-(function () {
-
-    const btn = document.createElement('button');
-    btn.innerHTML = '启用input';
-    btn.style.width = '100px';
-    btn.style.height = '40px';
-    btn.style.position = 'fixed';
-    btn.style.bottom = '60px';
-    btn.style.right = '20px';
-    btn.style.zIndex = '999999';
-    document.body.appendChild(btn);
-
-    window.onload = () => {
-        console.log('onload');
-        document.getElementById('finishedDate')?.removeAttribute('disabled');
-    };
-
-    btn.onclick = () => {
-        document.getElementById('finishedDate')?.removeAttribute('disabled');
-        // var formData = new FormData();
-        // formData.append('whatsapp_id', 8615768651563);
-        // formData.append('message', '{"body":"我是最新消息"}');
-        // formData.append('message_type', 'text');
-        // GM_xmlhttpRequest({
-        //     method: "POST",
-        //     url: "https://sales-export-dev.tangees.com/cgi/foreign-trade/api/wa/chat/send",
-        //     data: formData,
-        //     onload: function (response) {
-        //         console.log(JSON.parse(response?.response));
-        //     }
-        // });
-    };
-    // Your code here...
-}());
+"use strict";
+const W = typeof unsafeWindow === 'undefined' ? window : unsafeWindow;
+W.bliveproxy.addCommandHandler('ALL', command => {
+  if (command.cmd.startsWith('DANMU_MSG'))
+    command.cmd = 'DANMU_MSG';
+});
